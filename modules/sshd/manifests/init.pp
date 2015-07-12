5e30167955879d52f5bed349dc5302d8a55ccdd2
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-07-12 10:37:24 -0700

    try to fix instances

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -3,18 +3,20 @@ class sshd {
 		"openssh-server": ensure => installed;
 	}
 	file { "/etc/ssh/sshd_config":
-		source	=> ["puppet:///modules/sshd/sshd_config"],
+		source	=> ["puppet:///modules/sshd/$hostname/sshd_config",
+			    "puppet:///modules/sshd/sshd_config",],
 		mode	=> 444,
 		owner	=> root,
 		group	=> root,
 		require => Package["openssh-server"],
 	}
 	
-	service { "ssh":
+	service { "sshd":
 		enable		=> true,
 		ensure		=> running,
 		require		=> [ Package["openssh-server"],
-				     File["/etc/ssh/sshd_config"],],
+				     File["/etc/ssh/sshd_config"],
+				     Service["sshd"],],
 		subscribe	=> File["/etc/ssh/sshd_config"],
 	}
 }
