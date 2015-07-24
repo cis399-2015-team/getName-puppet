afcfffe672df6c036202f0ae650f62aa082c6243
Adam Lindsey <Adam@128-223-223-202-wireless.uoregon.edu>
2015-07-24 14:02:36 -0700

    update to puppet code for index.html page

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -2,16 +2,25 @@ class httpd{
 
 	package{ "postfix": ensure => installed;}
 	package{ "apache2": ensure => installed;}
+	file { "/var/www/html/index.html":
+		source	=> ["puppet:///modules/httpd/$hostname/index.html",
+			    "puppet:///modules/httpd/index.html",],
+		mode	=> 444,
+		owner	=> root,
+		group 	=> root,
+		require => Package["apache2"],
+	}
 	service{ "postfix":
                   enable  => true,
                   ensure  => running,
                   require => Package["postfix"],
         }   	
-
 	service{ "apache2":
                   enable  => true,
                   ensure  => running,
-                  require => Package["apache2"],
+                  require => [ Package["apache2"],
+			       File["/var/www/html/index.html"],],
+		  subscribe => File["/var/www/html/index.html"],
         } 
 }
 
