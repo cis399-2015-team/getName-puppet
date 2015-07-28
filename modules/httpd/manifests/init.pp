92ea4493a4aef3c38a8e23812c6f82f4c206c21a
Adam Lindsey <Adam@128-223-223-202-wireless.uoregon.edu>
2015-07-28 15:41:35 -0700

    fixes to puppet config for httpd module

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,18 +1,17 @@
 class httpd{
-
 	package{ "postfix": ensure => installed;}
 	package{ "apache2": ensure => installed;}
 	file { "/var/www/html/index.html":
-		source	=> ["puppet:///modules/httpd/$hostname/index.html",
-			    "puppet:///modules/httpd/index.html",],
+		source	=> ["puppet:///modules/httpd/files/$hostname/index.html",
+			    "puppet:///modules/httpd/files/index.html",],
 		mode	=> 444,
 		owner	=> root,
 		group 	=> root,
 		require => Package["apache2"],
 	}
 	file { "/etc/apache2/apache2.conf":
-		source	=> ["puppet:///modules/httpd/$hostname/apache2.conf",
-			    "puppet:///modules/httpd/apache2.conf",],
+		source	=> ["puppet:///modules/httpd/files/$hostname/apache2.conf",
+			    "puppet:///modules/httpd/files/apache2.conf",],
 		mode	=> 444,
 		owner	=> root,
 		group	=> root,
--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -2,7 +2,6 @@ class sshd {
 	package {
 		"openssh-server": ensure => installed;
 	}
-
 	ssh_authorized_key { "alindsey-Aeroponix":
 	user	=> "ubuntu",
 	key	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQDFP/RJsWcFJ4X04ZCLBxph2A1veyM8XDaBq6rfEG600U8cMesidfGFgvTVAgvnPDA0hlFWuqTl4ar5qKRsXwKyZXIGbdruhFlJYO0rhlqdgkQctEq80hbM2lexI+B57hbfwoAyaZYbjcEFhW6hIhjFhxWawQhEowMHplAGGqHYxk4B/HaI6VYheHkgq12bSIAnemglCwj5a1VDtX5DrJ5tdOUEgOGBE+jzbVg73jrg7e6lQejI5Zj9XyhfxAVSeDplOwByyrk2rCQQMthYmGV7pRgN1nkyI6zEaUtuXlz4egTfv/qa9Ol7NnWTzdmNljs5eRfJabue8tNPhMn65VQd",
@@ -15,10 +14,9 @@ class sshd {
 	user	=> "ubuntu",
 	key	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQCWKorntXMPYi8fgwNEjKxmOI4JnWnqaQoHQMWrZgsASdjJ1d4KAdWLoFQdo4WPT5mTTfEPYYubHUhFDOFpO1Vcx3mk5dub9bV5TfdUoovmO1sbNmWpwY28IH/Xy/gyeIls7dmVjJD6AkzPW9jT4vr6tfNaWjTOeeyG8ykKetuRFuiOYK9RIch5ovSwT60J1TY+alXZML6S7khGPYZXoMqNKMrLWtLhvG+/mQHS82AufTokb5MDydmCLcufs6F4NyCcUMSmfKrjjuU6pFqkzjwJ95gIzFAY7o4ifdAw0xb8e1rUaaEmaaZcvLhb3gq2jF419KiETKHXZbxyHRO6QiJH",
 	}
-
 	file { "/etc/ssh/sshd_config":
-		source	=> ["puppet:///modules/sshd/$hostname/sshd_config",
-			    "puppet:///modules/sshd/sshd_config",],
+		source	=> ["puppet:///modules/sshd/files/$hostname/sshd_config",
+			    "puppet:///modules/sshd/files/sshd_config",],
 		mode	=> 444,
 		owner	=> root,
 		group	=> root,
