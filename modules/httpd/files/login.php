95024c38b6055d9abd79fe365fe31c0597633e6d
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-08-08 14:48:31 -0700

    updates to php code

--- modules/httpd/files/login.php
+++ modules/httpd/files/login.php
@@ -1,5 +1,17 @@
 <?php
 
+$user = 'root';
+$password = 'getName399';
+$db = '';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysql_connect("$host:$port", $user, $password);
+$db_selected = mysql_select_db($db, $link);
+
+$SQLstring = "SELECT *";
+$query = mysql_query($SQLstring);
+
 function Login() {
 
 }
