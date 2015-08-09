b94c149fe09dfdf1a44e6ad27ab18d9f26170896
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-08-08 18:05:39 -0700

    updates to php verification code

--- modules/httpd/files/login.php
+++ modules/httpd/files/login.php
@@ -1,43 +1,43 @@
 <?php
 
 $user = 'root';
-$password = 'getName399';
-$db = '';
+$pass = 'getName399';
+$db = 'php';
 $host = 'localhost';
 $port = 8889;
-
-$link = mysql_connect("$host:$port", $user, $password);
+$link = mysql_connect("$host:$port", $user, $pass);
 $db_selected = mysql_select_db($db, $link);
 
-$SQLstring = "SELECT *";
-$query = mysql_query($SQLstring);
-
+/* function that handles logging into site */
 function Login() {
-
+	
 }
 
+/* function to check login status */
 function CheckLogin() {
-
+	
 }
 
-function CheckLoginDB() {
-
-}
-
-function RegisterUser() {
-
-}
-
-function SaveToDatabase() {
-
-}
-
-function CreateTable() {
-
-}
-
-function InsertIntoDB() {
-
+/* function to check login credentials against DB */
+function CheckLoginDB($name, $word) {
+	if(!$db_selected) { 
+		/* Return false if DB connection fails */
+		return false; 
+	}
+	
+	/* if connection if fine, query for the requested user */
+	$qry = "SELECT username, password FROM user ".
+		   "WHERE username='$name' AND password='$word' ";
+	$result = mysql_query($qry, $link);
+	
+	/* test user's authorization against DB */
+	if(!$result || mysql_num_rows($result) <= 0) {
+		/* return false if verification fails */
+		return false;
+	}
+	
+	/* user is verified if they exist in DB */
+	return true;
 }
 
 ?>
