8b211c2e60d827fcb19daf36f33cae4f15e58f97
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-08-10 20:41:27 -0700

    fixes

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,28 +7,24 @@ node ip-10-0-1-226 {
 	cron {"Instance update":
 	     command => "sudo apt-get update && sudo apt-get upgrade",
 	     user    => "root",
-             minute  => "*/1440",
-        }              
+         minute  => "*/1440",
+    }              
 	include sshd
 	include httpd
 	include postfixd
 	include apached
-	include phpd
-	include mysqld
 }
+
 node ip-10-0-1-138 {
 	include sshd
 	include httpd
 	include postfixd
 	include apached
-	include phpd
-	include mysqld
 }
+
 node ip-10-0-1-46 {
 	include sshd
 	include httpd
 	include postfixd
 	include apached
-	include phpd
-	include mysqld
 }
