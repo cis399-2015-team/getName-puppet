552689ac89c23ea34fc65bbfc455761560b9c7ba
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-08-10 20:26:12 -0700

    updates to php code

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,11 +1,11 @@
 class httpd{
 	file { "/var/www/html/index.html":
-		source	=> ["puppet:///modules/httpd/$hostname/index.html",
-			    "puppet:///modules/httpd/index.html",],
+		source	=> ["puppet:///modules/httpd/$hostname/index.php",
+			    "puppet:///modules/httpd/index.php",],
 		mode	=> 444,
 		owner	=> root,
 		group 	=> root,
-		require => Package["apache2"],
+		require => Package["php5"],
 	}
 	file { "/var/www/html/deny.jpg":
 		source	=> ["puppet:///modules/httpd/$hostname/deny.jpg",
