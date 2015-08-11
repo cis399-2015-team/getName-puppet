20374058589b7ef0202d7fb8674f8411997dfee6
Adam Lindsey <Adam@128-223-223-202-wireless.uoregon.edu>
2015-08-11 15:23:27 -0700

    redirection to https

--- modules/httpd/files/index.php
+++ modules/httpd/files/index.php
@@ -1,6 +1,11 @@
 <?php 
 
 include("../login.php");
+if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
+    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: $redirect");
+}
 
 ?>
 <html>
