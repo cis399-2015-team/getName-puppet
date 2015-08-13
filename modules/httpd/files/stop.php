2b8b18db80a6e593cf7918ccc7d8b388a0e2ccd4
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-08-12 22:10:28 -0700

    fixes

--- modules/httpd/files/stop.php
+++ modules/httpd/files/stop.php
@@ -33,7 +33,7 @@ session_start();
 	} 
 	if($_POST['gonow']) {
 		$id = $_SESSION['instance'];
-		$command = "sudo php ./button stop " . $id . " " . $region;
+		$command = "./button stop " . $id . " " . $region;
 		echo shell_exec($command);
 	}
 ?>
