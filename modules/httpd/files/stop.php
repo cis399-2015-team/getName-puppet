e7e95cf61b5684ece07d86dbd48fad1fb51357a5
Adam Lindsey <Adam@adam-lindseys-macbook-2.local>
2015-08-12 18:36:03 -0700

    updates to puppet code for instance operations

--- modules/httpd/files/stop.php
+++ modules/httpd/files/stop.php
@@ -16,10 +16,7 @@ $loc = 'https://ec2-52-10-36-255.us-west-2.compute.amazonaws.com/stop.php';
 <h1>Stop Instance <?php echo $_SESSION['instance']; ?></h1>
 
 <?php
-	//if($_POST['gonow']) {
-	//	$result = StopInstance($_SESSION['instance'], trim($_POST['key']), trim($_POST['secret']);
-	//	echo $result;
-	//} else {
+	if(!$_POST['gonow']) {
 		echo "<form id='stop' action=$loc method='post'>" . 
 		"<fieldset ><legend>Enter Credentials</legend>" .
 		"<label for='key' >KEY*:</label>" .
@@ -28,7 +25,10 @@ $loc = 'https://ec2-52-10-36-255.us-west-2.compute.amazonaws.com/stop.php';
 		"<input type='text' name='secret' id='secret' />" .
 		"<input type='submit' name='gonow' value='EXECUTE' />" .
 		"</fieldset></form>";
-	//}
+	} else {
+		$result = StopInstance($_SESSION['instance'], trim($_POST['key']), trim($_POST['secret']);
+		echo $result;
+	}
 ?>
 
 </body>
